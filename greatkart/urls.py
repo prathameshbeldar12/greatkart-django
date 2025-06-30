3a52b2baa746edc68ea283bcef0e0d64022c41c4
Prthamesh beldar <prathameshbeldar@gmail.com>
2025-06-30 23:20:46 +0530

    this is my first commit

---
+++ greatkart/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for greatkart project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django import views
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+from store import views as store_views  # Import the store view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('store.html', store_views.store, name='store_html'),  # <-- This line is key
+    path('store/', include('store.urls')),  # Optional: keep if you're also using /store/
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
